c896549732d814ead43428d0dbe04a83078b002f
thiagomaster20 <48996596+thiagomaster20@users.noreply.github.com>
2019-03-29 11:51:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def q_1():
                 nacionalidades.append(i[14])
     print("Total nacionalidades:", len(set(nacionalidades)))
     arq.close()
-
-
+    
 def q_2():
     arq = open('data.csv', encoding="utf8")
     linhas = csv.reader(arq)
@@ -22,7 +21,6 @@ def q_2():
                 clubes.append(i[3])
     print("Total clubes:", len(set(clubes)))
     arq.close()
-
 
 def q_3():
     arq = open('data.csv', encoding="utf8")
@@ -37,10 +35,8 @@ def q_3():
                 break
     arq.close()
 
-
 def q_4():
-    # A função sort - você altera a lista em si.
-    # A função sorted - você tem um valor que pode utilizar em uma variável nova.
+    
     arq = open('data.csv', encoding="utf8")
     linhas = csv.reader(arq)
     ct = 0
@@ -53,15 +49,15 @@ def q_4():
 
     for i in linhas:
         if not ('full_name' or 'eur_value') in i:
-            nome.append(i[2]) #Grava o nome dos jogadores.
-            aux2.append(i[17]) #Grava os salário dos jogadores porem em string.
-            valor.append(float(i[17])) #Grava os salário dos jogadores porem em inteiro.
+            nome.append(i[2]) 
+            aux2.append(i[17]) 
+            valor.append(float(i[17])) 
 
-    aux = sorted(valor, reverse=True) #Ordena a lista do menor ao maior valor.
+    aux = sorted(valor, reverse=True) 
 
-    for i in range(len(aux2)):  # Percorre toda a lista aux2.
-        for j in range(len(aux2)):  # Percorre toda a lista aux2 novamente para verificar se existe mais de uma valor igual nos indices.
-            if str(aux[ct]) in aux2[j]:  # Verifica se existe o valor da lista a na posição ct, dentro da lista aux2 na posição j.
+    for i in range(len(aux2)): 
+        for j in range(len(aux2)): 
+            if str(aux[ct]) in aux2[j]: 
                 if not nome[j] in jogador:
                     jogador.append(nome[j])
                     salario.append(aux[ct])
@@ -70,7 +66,6 @@ def q_4():
             break
     for i in range(10):
         print('Salario:',salario[i], 'Jogador:',jogador[i])
-
     arq.close()
 
 def q_5():
@@ -85,11 +80,11 @@ def q_5():
     ct = 0
     for i in linhas:
         if not 'age' in i:
-            aux.append(int(i[6])) #Grava a idade do jogador em inteiro.
-            aux2.append(i[6]) #Grava a idade do jogador em string.
-            nome.append(i[2]) #Grava o nome dos jogadores.
+            aux.append(int(i[6])) 
+            aux2.append(i[6]) 
+            nome.append(i[2]) 
 
-    age = sorted(aux, reverse=True) #Ordena a lista do menor ao maior valor.
+    age = sorted(aux, reverse=True) 
 
     for i in range(len(aux2)):
         for j in range(len(aux2)):
@@ -131,15 +126,13 @@ def q_6():
     aux = sorted(age, reverse=True)
 
     for i in range(len(lista_idades)):
-        print(c,'anos - ', lista_idades[i])
-        #soma = soma + lista_idades[i]
-        c+=1
-    #print(soma)
+        print(c,'anos - ', lista_idades[i])        
+        c+=1    
     arq.close()
 
-# q_1()
-# q_2()
-# q_3()
-# q_4()
-# q_5()
+q_1()
+q_2()
+q_3()
+q_4()
+q_5()
 q_6()
